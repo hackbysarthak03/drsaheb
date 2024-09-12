d825ecdc311bf29434e23c77889de781bfffbe20
Sarthak Vishwakarma <vsarthak62@gmail.com>
2024-09-12 23:14:18 +0530

    tenth

--- drsaheb/settings.py
+++ drsaheb/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-l+=_+3sh3yy_!2&r(ra@z)$+qh3gzh56^@-j=c+p%7ntad+##q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
